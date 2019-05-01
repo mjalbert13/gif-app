0c83186b32e0966f049ebc5276a3c06ee0872154
MattJ <47984960+mjalbert13@users.noreply.github.com>
2019-05-01 15:11:10 -0400

    logic finnished ajax working

--- assests/javascript/gifapp.js
+++ assests/javascript/gifapp.js
@@ -1,3 +1,77 @@
 var tvShows = ["Brooklyn 99", "The Office", "Arrested Devlopment","Parks and Recreation"];
+var buttonContainer = document.getElementById("new-buttons");
 
-var search =$(this).attr()
+
+$(document).ready(function(){
+// create buttons out of array and display them
+function renderButtons(){
+    $("#new-buttons").empty();
+
+    for(var i =0; i < tvShows.length; i++) {
+        var newButton = $("<button>");
+        newButton.addClass("btn btn-primary show");
+        newButton.attr("data-title",tvShows[i]);
+        newButton.text(tvShows[i]);
+        $("#new-buttons").append(newButton);
+        $("#user-input").text("");
+    }
+}
+
+
+//add new show to the array function
+$("#add-show").on("click", function(event){
+    event.preventDefault();
+
+    var show = $("#user-input").val();
+
+    tvShows.push(show);
+    
+    renderButtons();
+})
+
+//When you click on the button gifs will appear
+$(document).on("click",".show", function(){
+    //alert("you clicked a button")
+    var text= $(this).text();
+    console.log(text);
+ 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+text+"&api_key=aaiq9TI68egGEfdJ0DjHxEodRxp1StpD";
+
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response){
+        
+
+        var result = response.data;
+
+        for(var i =0; i< 10; i++){
+
+            var gifDiv = $("<div>");
+
+            gifDiv.addClass("gif-class");
+
+            var image = $("<img>");
+
+            image.attr("src",result[i].images.fixed_height.url);
+
+            $(gifDiv).append(image);
+
+            $("#gif-holder").prepend(gifDiv);
+        }
+
+        })
+})
+
+//clear all gifs
+$("#clear").on("click", function(){
+    $("#gif-holder").empty();
+});
+
+//$(document).on("click", ".show", showGif);
+
+renderButtons();
+});
+
+
+
